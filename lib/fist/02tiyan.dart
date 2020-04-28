66c0a691a2932e903bf3202f4aa76651665101ce
liuyusheng <2046432388@qq.com>
2020-04-28 10:00:28 +0800

    提价

--- lib/fist/02tiyan.dart
+++ lib/fist/02tiyan.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 main() => runApp(MyApp());
 
-//快捷键  option + enter 在return 的地方
-//查看抽象类的子类  option+command +B
-//
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -30,11 +27,14 @@ class HYhomePage extends StatelessWidget {
 class HyHomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String imageurl = "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2913529070,3813942882&fm=15&gp=0.jpg";
+    final String imageurl2 ="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588005049122&di=1cc31aa2a3c32dabbbdf093317aad20e&imgtype=0&src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F201701%2F24%2F20170124110951_nCvWX.thumb.700_0.jpeg";
+    final String imageUrl3 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588005228080&di=6db75a016ba3d4456aac3adf7023ebd7&imgtype=0&src=http%3A%2F%2Fcdn.duitang.com%2Fuploads%2Fitem%2F201409%2F08%2F20140908130732_kVXzh.jpeg";
     return ListView(
       children: <Widget>[
-        HyhomeProduct("Apple1","详情","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588002257841&di=6948bd92d157ca0f111f237a84c18076&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20180514%2F06%2F1526249163-XdGVHqcOYr.jpg"),
-        HyhomeProduct("Apple2","haha","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588002257841&di=6948bd92d157ca0f111f237a84c18076&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20180514%2F06%2F1526249163-XdGVHqcOYr.jpg"),
-        HyhomeProduct("apple3","你好","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588002257841&di=6948bd92d157ca0f111f237a84c18076&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20180514%2F06%2F1526249163-XdGVHqcOYr.jpg")
+        HyhomeProduct("Apple1","详情",imageurl),
+        HyhomeProduct("Apple2","haha",imageurl2),
+        HyhomeProduct("apple3","你好",imageUrl3),
       ],
     );
   }
@@ -61,9 +61,14 @@ class HyhomeProduct extends StatelessWidget {
           )
       ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          Text(this.title,style: style1),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(this.title,style: style1),
+            ],
+          ),
           SizedBox(height: 8),
           Text(this.desc,style: style2),
           SizedBox(height: 8),
--- lib/main.dart
+++ lib/main.dart
@@ -24,50 +24,25 @@ class HYhomePage extends StatelessWidget {
   }
 }
 
-class HyHomeContent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        HyhomeProduct("Apple1","详情","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588002257841&di=6948bd92d157ca0f111f237a84c18076&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20180514%2F06%2F1526249163-XdGVHqcOYr.jpg"),
-        HyhomeProduct("Apple2","haha","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588002257841&di=6948bd92d157ca0f111f237a84c18076&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20180514%2F06%2F1526249163-XdGVHqcOYr.jpg"),
-        HyhomeProduct("apple3","你好","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588002257841&di=6948bd92d157ca0f111f237a84c18076&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20180514%2F06%2F1526249163-XdGVHqcOYr.jpg")
-      ],
-    );
+
+class HyHomeContent  extends StatefulWidget
+{
+ @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
   }
 }
 
+class _HyHomeContentState extends State<HyHomeContent>
+{
 
-class HyhomeProduct extends StatelessWidget {
-  final  String   title;
-  final  String   desc;
-  final  String   imageURL;
+  String   title = "why";
 
-  final style1 = TextStyle(fontSize: 24,color: Colors.orange);
-  final style2 = TextStyle(fontSize: 20,color: Colors.green);
-
-  HyhomeProduct(this.title,this.desc,this.imageURL);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 1,//边框宽度
-          color: Colors.black12,
-        )
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(this.title,style: style1),
-          SizedBox(height: 8),
-          Text(this.desc,style: style2),
-          SizedBox(height: 8),
-          Image.network(imageURL)
-        ],
-      ),
-    );
+    // TODO: implement build
+    return Text("");
   }
 }
 
